ef0f238a70c2ac1d51e4c7442be88b9bc54f9394
Adeniyii <ifedayoadeniyi@gmail.com>
2023-05-30 21:32:01 +0100

    test

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,10 +10,5 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-
-	size_t current_depth = binary_tree_depth(tree->parent);
-
-	return (1 + current_depth);
+	return (tree->n);
 }
