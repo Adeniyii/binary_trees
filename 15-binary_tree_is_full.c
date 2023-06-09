a17709491736562041a3a0bcd659dd1ae31beb35
Adeniyii <ifedayoadeniyi@gmail.com>
2023-06-09 21:32:49 +0100

    fix weird code duplication

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,28 +8,6 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	size_t left_leaf, right_leaf, tmp;
-
-	if (tree == NULL)
-		return (0);
-
-	left_leaf = binary_tree_is_full(tree->left);
-	right_leaf = binary_tree_is_full(tree->right);
-
-	tmp = (left_leaf ^ right_leaf);
-
-	return (tmp == 0 ? 1 : 0);
-}
-#include "binary_trees.h"
-
-/**
- * binary_tree_is_full - checks if a binary tree is full
- * @tree: pointer to the root node of the tree to check
- *
- * Return: 1 if full, 0 if not or if tree is NULL
- */
-int binary_tree_is_full(const binary_tree_t *tree)
-{
 	int left_leaf, right_leaf;
 
 	if (tree == NULL)
